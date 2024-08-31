d480deec36ccd78bed74726b8ddb014473bcfc64
larry karani <larry@larrys-MacBook-Pro.local>
2024-08-31 23:14:49 +0300

    built the issues status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,24 @@
+import { Status } from "@prisma/client";
+import React from "react";
+import { Badge } from "@radix-ui/themes";
+
+interface IssueStatusBadgeProps {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "green" | "violet" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "green" },
+  CLOSED: { label: "Closed", color: "violet" },
+};
+
+const IssueStatusBadge = ({ status }: IssueStatusBadgeProps) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadge;
