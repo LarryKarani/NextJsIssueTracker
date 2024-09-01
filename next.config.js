eea7f8974f98b4fd50a6fcb4fc42334760845669
larry karani <larry@larrys-MacBook-Pro.local>
2024-09-01 15:11:57 +0300

    fix avator not loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'refferrer-policy', value: 'no-referrer'
+          }
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
