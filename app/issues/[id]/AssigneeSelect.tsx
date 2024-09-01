9673335432795069b588121b78c2dffe45ceb85e
larry karani <larry@larrys-MacBook-Pro.local>
2024-09-01 20:46:05 +0300

    refactor assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,18 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { Select } from "@radix-ui/themes";
 import { Issue, User } from "@prisma/client";
-import axios from "axios";
+import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
-import Skeleton from "react-loading-skeleton";
+import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
+import Skeleton from "react-loading-skeleton";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
     isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      return data;
-    },
-    staleTime: 1000 * 60 * 5,
-    retry: 3,
-  });
+  } = useUsers();
 
   const handleAssigneeChange = async (userId: string) => {
     try {
@@ -63,5 +54,17 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () => {
+  return useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      return data;
+    },
+    staleTime: 1000 * 60 * 5,
+    retry: 3,
+  });
+}
 
 export default AssigneeSelect;
