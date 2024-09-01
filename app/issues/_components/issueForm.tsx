078aafb01a28c3c347f34b14b5fb9842aa83f844
larry karani <larry@larrys-MacBook-Pro.local>
2024-09-01 10:22:48 +0300

    refactor:build edit details page

---
+++ app/issues/_components/issueForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { ErrorMessage } from "@/app/components";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, TextField, Spinner } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const NewIssueForm = ({issue}: {issue?: Issue}) => {
+  const [errorMessages, setErrorMessages] = React.useState<string | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const router = useRouter();
+
+  const handleCreateIssue = async (data: IssueFormData) => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/issues", data);
+      if (response.status === 201) {
+        setLoading(false);
+        router.push("/issues");
+      }
+    } catch (error) {
+      setLoading(false);
+      setErrorMessages("An error occurred while creating the issue.");
+    }
+  };
+
+  return (
+    <div className="max-w-xl">
+      {errorMessages && (
+        <Callout.Root className="mb-5" color="red">
+          <Callout.Text>{errorMessages}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => handleCreateIssue(data))}
+      >
+        <TextField.Root
+          defaultValue={issue?.title}
+          placeholder="Create New Issue"
+          {...register("title")}
+        />
+        {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
+        <Controller
+          name="description"
+          control={control}
+          defaultValue={issue?.description}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Description" />
+          )}
+        />
+        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
+        <Button disabled={loading}>
+          Submit Issue {loading && <Spinner />}{" "}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssueForm;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,73 +1,10 @@
-"use client";
+import React from 'react';
+import NewIssueForm from '../_components/issueForm';
 
-import { ErrorMessage } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import React from "react";
-import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
-import { z } from "zod";
-
-
-type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
-  const [errorMessages, setErrorMessages] = React.useState<string | null>(null);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-  const router = useRouter();
-
-  const handleCreateIssue = async (data: IssueForm) => {
-    try {
-      setLoading(true);
-      const response = await axios.post("/api/issues", data);
-      if (response.status === 201) {
-        setLoading(false);
-        router.push("/issues");
-      }
-    } catch (error) {
-      setLoading(false);
-      setErrorMessages("An error occurred while creating the issue.");
-    }
-  };
-
   return (
-    <div className="max-w-xl">
-      {errorMessages && (
-        <Callout.Root className="mb-5" color="red">
-          <Callout.Text>{errorMessages}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit((data) => handleCreateIssue(data))}
-      >
-        <TextField.Root placeholder="Create New Issue" {...register("title")} />
-        {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE {...field} placeholder="Description" />
-          )}
-        />
-        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
-        <Button disabled={loading}>
-          Submit Issue {loading && <Spinner />}{" "}
-        </Button>
-      </form>
-    </div>
-  );
-};
+    <NewIssueForm/>
+  )
+}
 
 export default NewIssuePage;
