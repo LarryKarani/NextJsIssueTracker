81cd0f97833366061a001fd8bd0e36e2d86c166f
larry karani <larry@larrys-MacBook-Pro.local>
2024-09-01 20:40:12 +0300

    show toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,51 +1,67 @@
-'use client';
+"use client";
 
-import React, {useEffect} from 'react';
-import { Select } from '@radix-ui/themes';
-import { Issue, User } from '@prisma/client';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query'
-import Skeleton from 'react-loading-skeleton';
+import React, { useEffect } from "react";
+import { Select } from "@radix-ui/themes";
+import { Issue, User } from "@prisma/client";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Skeleton from "react-loading-skeleton";
+import toast, { Toaster } from "react-hot-toast";
 
-const AssigneeSelect = ({issue}: {issue: Issue }) => {
-
-  const {data: users, error, isLoading} = useQuery<User[]>({
-    queryKey: ['users'],
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
     queryFn: async () => {
-      const {data} = await axios.get<User[]>('/api/users');
+      const { data } = await axios.get<User[]>("/api/users");
       return data;
     },
     staleTime: 1000 * 60 * 5,
-    retry: 3
-  })
+    retry: 3,
+  });
 
-  const handleAssigneeChange = (userId: string) => {
-    axios.patch(`/api/issues/${issue.id}`, {assignedToUserId: userId === "unassigned" ? null : userId})
-  }
+  const handleAssigneeChange = async (userId: string) => {
+    try {
+      await axios.patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      });
+    } catch (error) {
+      toast.error("Changes could not be saved");
+      console.error(error);
+    }
+  };
 
-  if(isLoading) return <Skeleton/>
+  if (isLoading) return <Skeleton />;
 
-  if (error) return null
+  if (error) return null;
 
   return (
-    <Select.Root 
-    defaultValue={issue.assignedToUserId ? issue.assignedToUserId : "unassigned"}
-    onValueChange={(userId) => handleAssigneeChange(userId)}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value={"unassigned"}>Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={
+          issue.assignedToUserId ? issue.assignedToUserId : "unassigned"
+        }
+        onValueChange={(userId) => handleAssigneeChange(userId)}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value={"unassigned"}>Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
-}
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
