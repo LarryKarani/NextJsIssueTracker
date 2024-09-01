7c6546bd2877a34589545c4ebe5a0803b31ddd7b
larry karani <larry@larrys-MacBook-Pro.local>
2024-09-01 14:38:25 +0300

    add login and logout link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 
 const NavBar = () => {
 
   const currentPath = usePathname()
-  
-
+  const { status, data: session } = useSession()
   const links = [{label: 'Dashboard', href: '/'}, {label: 'Issues', href: '/issues/list'}]
   return (
     <nav className="flex space-x-6 px-5 items-center border-b h-14 mb-5">
@@ -32,6 +33,18 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Box>
+            <Link href="/api/auth/signout">Sign Out</Link>
+          </Box>
+        )}
+        {status === 'unauthenticated' && (
+          <Box>
+            <Link href="/api/auth/signin">Sign In</Link>
+          </Box>
+        )}
+      </Box>
     </nav>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import "@radix-ui/themes/styles.css"
-import './theme.config.css'
-import "./globals.css";
+import { Container, Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, Container } from "@radix-ui/themes";
 import NavBar from "./NavBar";
-
+import AuthProvider from "./auth/Provider";
+import "./globals.css";
+import "./theme.config.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -22,12 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <Theme accentColor="iris">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="iris">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
